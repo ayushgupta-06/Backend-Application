4022e7cbc5e98495a292f45eabc45fb3fcb96f01
Ayush Gupta <ayushgupta2k7@gmail.com>
2025-02-27 11:33:40 +0530

    utils updated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ dotenv.config({
   path: './.env',
 });
 import connectDB from './db/db.js';
+import { app } from './app.js';
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Error: ', err);
+    process.exit(1);
+  });
